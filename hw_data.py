189a4a88c0df032c300162f5df8705230ec026ec
SuhaniRana <70816383+SuhaniRana@users.noreply.github.com>
2021-06-01 17:14:49 -0400

    Add files via upload

---
+++ hw_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import csv
+import plotly.graph_objects as go
+import plotly.express as px
+df = pd.read_csv("data.csv")
+student_df = df.loc[df['student_id']=="TRL_xsl"]
+print(student_df.groupby("level")["attempt"].mean())
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
+fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
+fig.show()
